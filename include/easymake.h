b6836a546e95fdcb62f2abf84e1925896ac2aab9
undersquire <apexdev23@gmail.com>
2020-11-30 11:21:55 -0800

    Started writing easymake

--- include/easymake.h
+++ include/easymake.h
@@ -1,6 +1,18 @@
 #ifndef EASYMAKE_H
 #define EASYMAKE_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct
+{
+  char *description, compiler, output_file;
+  char **sources, includes, libraries, compiler_options;
+  int sources_count, includes_count, libraries_count, compiler_options_count;
+} BuildOptions;
+
+char *easymake_read_file(char *file);
+BuildOptions easymake_build_options(char *buf);
+void easymake_build_project(BuildOptions *build_options);
 
 #endif
--- src/easymake.c
+++ src/easymake.c
@@ -1,6 +1,72 @@
 #include "easymake.h"
 
+float VERSION = 0.1;
+
+char *easymake_read_file(char *file)
+{
+  FILE *file;
+  file = fopen(file, "rb");
+  
+  if(!file)
+  {
+    prinf("easymake: file not found!\n");
+    return;
+  }
+  
+  fseek(file, 0, SEEK_END);
+  
+  long length = ftell(file);
+  rewind(file);
+  
+  char *text = calloc(1, length + 1);
+  
+  if(!text)
+  {
+    fclose(file);
+    
+    printf("easymake: memory error\n");
+    return;
+  }
+  
+  if(!fread(text, length, 1, file))
+  {
+    free(text);
+    fclose(file);
+    
+    printf("easymake: failed to read build file!\n");
+    return 0;
+  }
+  
+  fclose(file);
+  return text;
+}
+
+BuildOptions easymake_build_options(char *buf)
+{
+  BuildOptions boptions;
+  
+  // @TODO parse build file and genereate build options
+  
+  free(buf);
+  return boptions;
+}
+
+void easymake_build_project(BuildOptions *boptions)
+{
+  // @TODO run build process based on build options
+}
+
 int main(int argc, char **argv)
 {
+  if(argc > 1)
+  {
+    // lol
+    easymake_build_project(&easymake_build_options(easymake_read_file(argv[1]));
+  }
+  else
+  {
+    printf("easymake v%.1f - by undersquire\n", VERSION);
+  }
   
+  return 0;
 }
