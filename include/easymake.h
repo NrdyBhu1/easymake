8086e6e947403910570fb194fd29fb37d77ecb89
undersquire <apexdev23@gmail.com>
2020-11-30 13:31:21 -0800

    Started JSON parsing
    Not usable quite yet

--- include/easymake.h
+++ include/easymake.h
@@ -2,12 +2,13 @@
 #define EASYMAKE_H
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 typedef struct
 {
-  char *description, compiler, output_file;
-  char **sources, includes, libraries, compiler_options;
+  const char *project, *compiler, *output;
+  const char **sources, **includes, **libraries, **compiler_options;
   int sources_count, includes_count, libraries_count, compiler_options_count;
 } BuildOptions;
 
--- src/easymake.c
+++ src/easymake.c
@@ -1,16 +1,17 @@
 #include "easymake.h"
+#include "json.h"
 
 float VERSION = 0.1;
 
-char *easymake_read_file(char *file)
+char *easymake_read_file(char *file_path)
 {
   FILE *file;
-  file = fopen(file, "rb");
+  file = fopen(file_path, "rb");
   
   if(!file)
   {
-    prinf("easymake: file not found!\n");
-    return;
+    printf("easymake: file not found!\n");
+    return NULL;
   }
   
   fseek(file, 0, SEEK_END);
@@ -25,7 +26,7 @@ char *easymake_read_file(char *file)
     fclose(file);
     
     printf("easymake: memory error\n");
-    return;
+    return NULL;
   }
   
   if(!fread(text, length, 1, file))
@@ -34,7 +35,7 @@ char *easymake_read_file(char *file)
     fclose(file);
     
     printf("easymake: failed to read build file!\n");
-    return 0;
+    return NULL;
   }
   
   fclose(file);
@@ -46,6 +47,65 @@ BuildOptions easymake_build_options(char *buf)
   BuildOptions boptions;
   
   // @TODO parse build file and genereate build options
+  struct json_value_s *root = json_parse(buf, strlen(buf));
+  
+  if(root->type == json_type_object)
+  {
+    struct json_object_s *object = (struct json_object_s *)root;
+    
+    struct json_object_element_s *key = object->start;
+    
+    while(key != NULL)
+    {
+      struct json_string_s *str = key->name;
+      if(strcmp(str->string, "project"))
+      {
+        struct json_value_s *val = key->value;
+        
+        if(val->type == json_type_string)
+        {
+          struct json_string_s *strval = (struct json_string_s *)val;
+          boptions.project = strval->string;
+        }
+      }
+      else if(strcmp(str->string, "compiler"))
+      {
+        struct json_value_s *val = key->value;
+        
+        if(val->type == json_type_string)
+        {
+          struct json_string_s *strval = (struct json_string_s *)val;
+          boptions.compiler = strval->string;
+        }
+      }
+      else if(strcmp(str->string, "output"))
+      {
+        struct json_value_s *val = key->value;
+        
+        if(val->type == json_type_string)
+        {
+          struct json_string_s *strval = (struct json_string_s *)val;
+          boptions.output = strval->string;
+        }
+      }
+      else if(strcmp(str->string, "includes"))
+      {
+        struct json_value_s *val = key->value;
+        
+        if(val->type == json_type_array)
+        {
+          struct json_array_s *array = (struct json_array_s *)val;
+          
+          char **includes = (char **)malloc(sizeof(char *) * array->length);
+          
+        }
+      }
+    }
+  }
+  else
+  {
+    printf("easymake: invalid json!\n");
+  }
   
   free(buf);
   return boptions;
@@ -56,16 +116,33 @@ void easymake_build_project(BuildOptions *boptions)
   // @TODO run build process based on build options
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
   if(argc > 1)
   {
-    // lol
-    easymake_build_project(&easymake_build_options(easymake_read_file(argv[1]));
+    // @TODO why isnt this string comparison working??
+    if(strcmp(argv[1], "-version"))
+    {
+      printf("easymake v%.1f - by undersquire\n", VERSION);
+    }
+    else
+    {
+      char *buf = easymake_read_file(argv[1]);
+      
+      if(!buf) return 0;
+      
+      BuildOptions boptions = easymake_build_options(buf);
+      easymake_build_project(&boptions);
+    }
   }
   else
   {
-    printf("easymake v%.1f - by undersquire\n", VERSION);
+    char *buf = easymake_read_file("build.ezmk");
+    
+    if(!buf) return 0;
+    
+    BuildOptions boptions = easymake_build_options(buf);
+    easymake_build_project(&boptions);
   }
   
   return 0;
