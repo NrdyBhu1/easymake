d3f69d3ad4da9a3e695ab3bae04a05e2bf47df11
undersquire <apexdev23@gmail.com>
2020-12-03 12:43:36 -0800

    Update easyjson.h

--- include/easyjson.h
+++ include/easyjson.h
@@ -165,6 +165,9 @@ void json_free(struct json_object *object)
   int i;
   for(i = 0; i < object->length; i++)
   {
+    if(object->values[i]->type == json_type_object
+      || object->values[i]->type == json_type_array) 
+        json_free((struct json_object *)object->values[i]);
     free(object->values[i]);
   }
   free(object->values);
